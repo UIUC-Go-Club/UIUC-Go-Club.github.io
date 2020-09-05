521ee6fcac4ef02f92d54c29a88a7439e2e0dbca
xingzhi <xingzhi2@illinois.edu>
2020-09-05 16:01:49 -0500

    first

---
+++ HowToCreateBlogs.md
@@ -0,0 +1,20 @@
+# How to write a blog with markdown and github pages
+### Tools:
+- [jkelly](https://jekyllrb.com/docs/)
+
+
+
+### Install *jkelly*:
+
+```{bash}
+# Add ruby to available pacakge list
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+
+# jekyll requires at ruby >= 2.4
+sudo apt-get install ruby2.7 ruby2.7-dev
+
+sudo gem install jekyll
+jekyll -v
+```
+
---
+++ blogs/imgs/crop.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+# Opens a image in RGB mode
+im = Image.open(r"eternal_life_demo_1.png")
+
+# Size of the image in pixels (size of orginal image)
+# (This is not mandatory)
+width, height = im.size
+
+# Setting the points for cropped image
+left = 5
+top = height / 4
+right = 164
+bottom = 3 * height / 4
+
+# Cropped image of above dimension
+# (It will not change orginal image)
+im1 = im.crop((left, top, right, bottom))
+
+# Shows the image in image viewer
+im1.show()
